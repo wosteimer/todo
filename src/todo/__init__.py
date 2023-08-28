2f12bd2cb6f6bdb8e1dd0f5292f24dd8b84ff9b8
William <wosteimer1@gmail.com>
2023-08-28 08:10:43 -0300

    making settings work by environment variables

--- src/todo/__init__.py
+++ src/todo/__init__.py
@@ -1,22 +1,26 @@
 from pathlib import Path
 
 from starlette.applications import Starlette
+from starlette.config import Config
 from starlette.templating import Jinja2Templates
 
 from .repository.sqlite.sqlite_repository import SqliteRepository
 from .routes import create_routes
 
-TEMPLATE_DIRECTORY = Path(__file__).parent / "template"
-DB_PATH = str(Path(__file__).parent / "todo.db")
+config = Config(".env")
+DEBUG = config("DEBUG", cast=bool, default=False)
+DATABASE_URL = config("DATABASE_URL", cast=str)
+TEMPLATE_DIRECTORY = str(Path(__file__).parent / "template")
 STATIC_FILES_DIRECTORY = str(Path(__file__).parent / "static")
+print(DATABASE_URL)
 
 
 def main():
     template = Jinja2Templates(
         directory=TEMPLATE_DIRECTORY, trim_blocks=True, lstrip_blocks=True
     )
-    repository = SqliteRepository.create(DB_PATH)
+    repository = SqliteRepository.create(DATABASE_URL)
     routes = create_routes(template, repository, STATIC_FILES_DIRECTORY)
-    app = Starlette(debug=True, routes=routes)
+    app = Starlette(debug=DEBUG, routes=routes)
 
     return app
